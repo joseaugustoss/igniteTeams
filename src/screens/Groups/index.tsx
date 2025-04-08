4053e0b59f4b76c4403ccfd01d1563aee303827c
José Augusto <joseaugusto0408@gmail.com>
2025-04-08 16:24:32 -0300

    path mapping

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View, Image} from 'react-native';
+import { styles } from './styles';
+
+export function Groups(){
+  return (
+    <View  style={styles.container}>
+  <Text> Groups </Text>
+  </View>
+  )
+}
