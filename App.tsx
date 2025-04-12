e2d79865a87ad1a279950337adea9fed7de3f34c
José Augusto <joseaugusto0408@gmail.com>
2025-04-12 11:58:02 -0300

    personal fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import { Groups } from '@screens/Groups';
 import {theme} from './src/theme';
 import { ThemeProvider } from 'styled-components';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-    <Groups />
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+  
+    return (
+    
+      <ThemeProvider theme={theme}>
+      {!fontsLoaded ? (
+        <ActivityIndicator
+          color={theme.COLORS.GREEN_500}
+          size="large"
+          style={{flex: 1, backgroundColor: theme.COLORS.GRAY_600}}
+        />
+      ) : (
+        <Groups />
+      )}
+      </ThemeProvider>
+    );
+  
 }
